2a0a44ca7946597713a280ce98bac13dcb5939c2
malakifiss <156612780+malakifiss@users.noreply.github.com>
2025-05-04 12:02:14 +0100

    Update CustomerRepository.java

--- src/main/java/org/sid/ebankingbackend/repositories/CustomerRepository.java
+++ src/main/java/org/sid/ebankingbackend/repositories/CustomerRepository.java
@@ -1,4 +1,12 @@
 package org.sid.ebankingbackend.repositories;
 
-public interface CustomerRepository {
+import org.sid.ebankingbackend.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+
 }
